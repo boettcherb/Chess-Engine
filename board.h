db8bd91205bc16a5d9065bdff9c24debb6f02d6a
Brandon Boettcher <brandonboettcher19@gmail.com>
2020-08-11 08:39:01 -0500

    Create board.h with the board struct
    Create a header file containing the main board struct. A chessboard
    in this engine will be represented using bitboards. Bitboards are
    generally faster than an array of 64 pieces because of bitwise
    operators. Most modern computers are 64-bit, which make bitwise
    operations on 64-bit numbers very fast. The board struct currently
    has only a few variables in it, but more will be added later.

---
+++ board.h
@@ -0,0 +1,36 @@
+
+#ifndef BOARD_H_INCLUDED
+#define BOARD_H_INCLUDED
+
+#include "defs.h"
+
+/*
+ * In this engine, a chessboard is represented using bitboards. Each bitboard
+ * is a 64-bit number where the least significant bit (bit 0) represents square
+ * A1 and the most significant bit (bit 63) represents square H8. A bit is set
+ * to '1' if there is a piece on that square.
+ * 
+ * pieceBitboards:    The bitboards for this chessboard. There are 12 of them,
+ *                    one for each piece type.
+ * colorBitboards:    3 bitboards to represent (1) all the white pieces, (2)
+ *                    all the black pieces, and (3) all pieces of both colors.
+ *                    These bitboards are used often and it is more efficient
+ *                    to keep them updated with the piece bitboards as moves
+ *                    are made then to calculate them when needed.
+ * pieces:            An array of 64 chars to hold the piece type for each
+ *                    square. This allows quick access of the piece type of a
+ *                    given square and is also updated incrementally with the
+ *                    piece bitboards.
+ * sideToMove:        An integer that is either 0 (white) or 1 (black).
+ * ply:               An integer holding the number of half moves made to get
+ *                    to the current board position.
+ */
+typedef struct {
+    uint64 pieceBitboards[NUM_PIECE_TYPES];
+    uint64 colorBitboards[3];
+    signed char pieces[64];
+    int sideToMove;
+    int ply;
+} Board;
+
+#endif
