49682af1c0c90094705185ad7987164031c1bdab
Brandon Boettcher <brandonb@cs.utexas.edu>
2020-09-15 18:40:15 -0500

    Remove getLSB() from attack.c

--- attack.c
+++ attack.c
@@ -2,25 +2,6 @@
 #include "defs.h"
 
 /*
- * Retrieve the index of the given bitboard's least significant bit. Ex: 
- * getLSB(0x1) = 0, getLSB(0x4) = 2, getLSB(0xC00) = 10. There is undefined
- * behavior if the bitboard is equal to 0.
- * 
- * bitboard:     The 64-bit number whose LSB index is retrieved. Must not be 0.
- * 
- * return:       The index of the given bitboard's least significant bit.
- */
-int getLSB(uint64 bitboard) {
-    assert(bitboard != 0);
-    int index = -1;
-    int found = 0;
-    while (!found) {
-        found = (bitboard >> ++index) & 0x1;
-    }
-    return index;
-}
-
-/*
  * For each index (0-63) there is a 1-bit in the positions that a king at that
  * index would be able to attack.
  * Remember: bit 0 = A1, bit 1 = B1, ... , bit 62 = G8, bit 63 = H8.
