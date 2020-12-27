28f647ebd8e7d88a34f40f34380114849f701b89
Brandon Boettcher <BrandonBoettcher19@gmail.com>
2020-12-27 15:47:20 -0600

    Implement sliding piece attack functions
    Implement getBishopAttacks, getRookAttacks, and getQueenAttacks in
    attack.c. These functions use blocker bitboards and magic numbers
    to retrieve an attack bitboard from the bishop and rook attack
    tables. They will be used in movegen.c to generate all the possible
    moves on the board.

--- attack.h
+++ attack.h
@@ -7,5 +7,8 @@ uint64 getKingAttacks(uint64 king);
 uint64 getKnightAttacks(uint64 knights);
 uint64 getPawnAttacksRight(uint64 pawns, int color);
 uint64 getPawnAttacksLeft(uint64 pawns, int color);
+uint64 getBishopAttacks(int square, uint64 blockers);
+uint64 getRookAttacks(int square, uint64 blockers);
+uint64 getQueenAttacks(int square, uint64 blockers);
 
 #endif
